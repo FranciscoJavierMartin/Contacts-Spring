25ef530199d69a4d3ea390c7b884caf85f1ffa8d
Francisco <javiermartincuenca@gmail.com>
2017-07-28 14:58:08 +0200

    Initial commit

---
+++ src/main/java/com/rathalos/contactschedule/contactSchedule/ContactScheduleApplication.java
@@ -0,0 +1,12 @@
+package com.rathalos.contactschedule.contactSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactScheduleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rathalos/contactschedule/contactSchedule/ContactScheduleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rathalos.contactschedule.contactSchedule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactScheduleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
